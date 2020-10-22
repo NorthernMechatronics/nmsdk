d0e473721691d4982f9b713a79948f4fe3b631ff
Joshua Wong <joshua@northernmechatronics.com>
2020-10-22 17:21:19 -0400

    Refactored code.

--- platform/console/iom_service.h
+++ platform/console/iom_service.h
@@ -1,12 +1,36 @@
 /*
- * iom.h
+ * BSD 3-Clause License
  *
- *  Created on: Jul 20, 2019
- *      Author: joshua
+ * Copyright (c) 2020, Northern Mechatronics, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-#ifndef _IOM_IOM_H_
-#define _IOM_IOM_H_
+#ifndef _IOM_H_
+#define _IOM_H_
 
 #if defined(__cplusplus)
 extern "C" {
@@ -14,10 +38,10 @@ extern "C" {
 
 extern TaskHandle_t iom_task_handle;
 
-void g_iom_task(void *pvp);
+void iom_task(void *pvp);
 
 #if defined(__cplusplus)
 }
 #endif  // defined(__cplusplus)
 
-#endif /* _IOM_IOM_H_ */
+#endif /* _IOM_H_ */
