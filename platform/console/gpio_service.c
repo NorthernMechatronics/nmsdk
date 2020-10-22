60d54d788cb66d32282e3574f2dd6288bd71ef0d
Joshua Wong <joshua@northernmechatronics.com>
2020-10-22 17:15:27 -0400

    refactored gpio.

--- platform/console/gpio_service.c
+++ platform/console/gpio_service.c
@@ -1,20 +1,39 @@
 /*
- * Gpio.c
+ * BSD 3-Clause License
  *
- *  Created on: Jul 20, 2019
- *      Author: joshua
+ * Copyright (c) 2020, Northern Mechatronics, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
 
-//*****************************************************************************
-//
-// Standard AmbiqSuite includes.
-//
-//*****************************************************************************
 #include <am_mcu_apollo.h>
 #include <am_util.h>
 
@@ -435,7 +454,7 @@ portBASE_TYPE prvGpioCommand(char *pcWriteBuffer, size_t xWriteBufferLen, const 
 	return xReturn;
 }
 
-void g_gpio_task(void *pvp)
+void gpio_task(void *pvp)
 {
     FreeRTOS_CLIRegisterCommand(&prvGpioCommandDefinition);
     vTaskDelete(NULL);
