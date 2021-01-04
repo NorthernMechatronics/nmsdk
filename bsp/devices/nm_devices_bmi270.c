d31a03c7720d39de24651a76cad94ebfdaf1a7a9
Joshua Wong <joshua@northernmechatronics.com>
2021-01-04 12:39:40 -0500

    Added BMI270 driver file.

---
+++ bsp/devices/nm_devices_bmi270.c
@@ -0,0 +1,208 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <am_mcu_apollo.h>
+#include <am_util.h>
+
+#include <bmi2_defs.h>
+
+static uint8_t dev_addr;
+
+int8_t bmi2_interface_init(struct bmi2_dev *bmi, uint8_t intf)
+{
+    int8_t rslt = BMI2_OK;
+
+    return rslt;
+}
+
+void bmi2_error_codes_print_result(int8_t rslt)
+{
+    switch (rslt)
+    {
+        case BMI2_OK:
+            /* Do nothing */
+            break;
+
+        case BMI2_W_FIFO_EMPTY:
+            am_util_stdio_printf("Warning [%d] : FIFO empty\r\n", rslt);
+            break;
+        case BMI2_W_PARTIAL_READ:
+            am_util_stdio_printf("Warning [%d] : FIFO partial read\r\n", rslt);
+            break;
+        case BMI2_E_NULL_PTR:
+            am_util_stdio_printf(
+                "Error [%d] : Null pointer error. It occurs when the user tries to assign value (not address) to a pointer," " which has been initialized to NULL.\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_COM_FAIL:
+            am_util_stdio_printf(
+                "Error [%d] : Communication failure error. It occurs due to read/write operation failure and also due " "to power failure during communication\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DEV_NOT_FOUND:
+            am_util_stdio_printf("Error [%d] : Device not found error. It occurs when the device chip id is incorrectly read\r\n",
+                   rslt);
+            break;
+
+        case BMI2_E_INVALID_SENSOR:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid sensor error. It occurs when there is a mismatch in the requested feature with the " "available one\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SELF_TEST_FAIL:
+            am_util_stdio_printf(
+                "Error [%d] : Self-test failed error. It occurs when the validation of accel self-test data is " "not satisfied\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_INT_PIN:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid interrupt pin error. It occurs when the user tries to configure interrupt pins " "apart from INT1 and INT2\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_OUT_OF_RANGE:
+            am_util_stdio_printf(
+                "Error [%d] : Out of range error. It occurs when the data exceeds from filtered or unfiltered data from " "fifo and also when the range exceeds the maximum range for accel and gyro while performing FOC\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ACC_INVALID_CFG:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid Accel configuration error. It occurs when there is an error in accel configuration" " register which could be one among range, BW or filter performance in reg address 0x40\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_GYRO_INVALID_CFG:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid Gyro configuration error. It occurs when there is a error in gyro configuration" "register which could be one among range, BW or filter performance in reg address 0x42\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ACC_GYR_INVALID_CFG:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid Accel-Gyro configuration error. It occurs when there is a error in accel and gyro" " configuration registers which could be one among range, BW or filter performance in reg address 0x40 " "and 0x42\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_CONFIG_LOAD:
+            am_util_stdio_printf(
+                "Error [%d] : Configuration load error. It occurs when failure observed while loading the configuration " "into the sensor\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_PAGE:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid page error. It occurs due to failure in writing the correct feature configuration " "from selected page\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SET_APS_FAIL:
+            am_util_stdio_printf(
+                "Error [%d] : APS failure error. It occurs due to failure in write of advance power mode configuration " "register\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_AUX_INVALID_CFG:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid AUX configuration error. It occurs when the auxiliary interface settings are not " "enabled properly\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_AUX_BUSY:
+            am_util_stdio_printf(
+                "Error [%d] : AUX busy error. It occurs when the auxiliary interface buses are engaged while configuring" " the AUX\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_REMAP_ERROR:
+            am_util_stdio_printf(
+                "Error [%d] : Remap error. It occurs due to failure in assigning the remap axes data for all the axes " "after change in axis position\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_GYR_USER_GAIN_UPD_FAIL:
+            am_util_stdio_printf(
+                "Error [%d] : Gyro user gain update fail error. It occurs when the reading of user gain update status " "fails\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SELF_TEST_NOT_DONE:
+            am_util_stdio_printf(
+                "Error [%d] : Self-test not done error. It occurs when the self-test process is ongoing or not " "completed\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_INPUT:
+            am_util_stdio_printf("Error [%d] : Invalid input error. It occurs when the sensor input validity fails\r\n", rslt);
+            break;
+
+        case BMI2_E_INVALID_STATUS:
+            am_util_stdio_printf("Error [%d] : Invalid status error. It occurs when the feature/sensor validity fails\r\n", rslt);
+            break;
+
+        case BMI2_E_CRT_ERROR:
+            am_util_stdio_printf("Error [%d] : CRT error. It occurs when the CRT test has failed\r\n", rslt);
+            break;
+
+        case BMI2_E_ST_ALREADY_RUNNING:
+            am_util_stdio_printf(
+                "Error [%d] : Self-test already running error. It occurs when the self-test is already running and " "another has been initiated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_CRT_READY_FOR_DL_FAIL_ABORT:
+            am_util_stdio_printf(
+                "Error [%d] : CRT ready for download fail abort error. It occurs when download in CRT fails due to wrong " "address location\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DL_ERROR:
+            am_util_stdio_printf(
+                "Error [%d] : Download error. It occurs when write length exceeds that of the maximum burst length\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_PRECON_ERROR:
+            am_util_stdio_printf(
+                "Error [%d] : Pre-conditional error. It occurs when precondition to start the feature was not " "completed\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ABORT_ERROR:
+            am_util_stdio_printf("Error [%d] : Abort error. It occurs when the device was shaken during CRT test\r\n", rslt);
+            break;
+
+        case BMI2_E_WRITE_CYCLE_ONGOING:
+            am_util_stdio_printf(
+                "Error [%d] : Write cycle ongoing error. It occurs when the write cycle is already running and another " "has been initiated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ST_NOT_RUNING:
+            am_util_stdio_printf(
+                "Error [%d] : Self-test is not running error. It occurs when self-test running is disabled while it's " "running\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DATA_RDY_INT_FAILED:
+            am_util_stdio_printf(
+                "Error [%d] : Data ready interrupt error. It occurs when the sample count exceeds the FOC sample limit " "and data ready status is not updated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_FOC_POSITION:
+            am_util_stdio_printf(
+                "Error [%d] : Invalid FOC position error. It occurs when average FOC data is obtained for the wrong" " axes\r\n",
+                rslt);
+            break;
+
+        default:
+            am_util_stdio_printf("Error [%d] : Unknown error code\r\n", rslt);
+            break;
+    }
+}
+
