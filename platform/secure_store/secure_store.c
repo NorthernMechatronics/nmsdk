7a2204a0fe834c091d5c6910311b8ed6b1601890
Joshua Wong <joshua@northernmechatronics.com>
2021-06-07 00:50:28 -0400

    Corrected master key address

--- platform/secure_store/secure_store.c
+++ platform/secure_store/secure_store.c
@@ -1,3 +1,39 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Northern Mechatronics, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <am_mcu_apollo.h>
 #include "secure_store_config.h"
 #include "secure_store.h"
@@ -8,8 +44,8 @@
 
 static void secure_store_master_key_read(uint8_t *key)
 {
-    uint32_t *address = key;
-    uint32_t *source = MASTER_KEY_ADDRESS;
+    uint32_t *address = (uint32_t *)key;
+    uint32_t *source = (uint32_t *)MASTER_KEY_ADDRESS;
     for (int i = 0; i < MASTER_KEY_SIZE; i += 4)
     {
         uint32_t value = am_hal_flash_load_ui32(source);
@@ -26,13 +62,13 @@ void secure_store_unlock()
     uint32_t *address, *value;
 
     secure_store_master_key_read(master_key);
-    address = SECURE_STORE_UNLOCK_CMD_ADDR;
+    address = (uint32_t *)SECURE_STORE_UNLOCK_CMD_ADDR;
     am_hal_flash_store_ui32(address, 0x01);
 
-    address = SECURE_STORE_UNLOCK_KEY_ADDR;
+    address = (uint32_t *)SECURE_STORE_UNLOCK_KEY_ADDR;
     for (int i = 0; i < MASTER_KEY_SIZE; i += 4)
     {
-        value = &(master_key[i]);
+        value = (uint32_t *)(&(master_key[i]));
         am_hal_flash_store_ui32(address, *value);
         address++;
     }
@@ -42,7 +78,7 @@ void secure_store_lock()
 {
     uint32_t *address;
 
-    address = SECURE_STORE_UNLOCK_KEY_ADDR;
+    address = (uint32_t *)SECURE_STORE_UNLOCK_KEY_ADDR;
     for (int i = 0; i < MASTER_KEY_SIZE; i += 4)
     {
         am_hal_flash_store_ui32(address, 0xFFFFFFFF);
@@ -59,7 +95,7 @@ void secure_store_read(uint32_t addr, uint8_t *target, size_t len)
 
     for (int i = 0; i < len; i += 4)
     {
-        uint32_t value = am_hal_flash_load_ui32(src);
+        uint32_t value = am_hal_flash_load_ui32((uint32_t *)src);
         memcpy(dest, (uint8_t *)(&value), 4);
 
         src  += 4;
